fa2ff8ae91fcb7ed3791b8f352593092c6af889c
Garrett Johnson <garrettrayj@gmail.com>
2018-10-20 15:56:43 -0600

    Initial Commit

---
+++ ReloadAll/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReloadAll
+//
+//  Created by Garrett Johnson on 10/20/18.
+//  Copyright © 2018 DevSci. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ReloadAllTests/ReloadAllTests.swift
@@ -0,0 +1,34 @@
+//
+//  ReloadAllTests.swift
+//  ReloadAllTests
+//
+//  Created by Garrett Johnson on 10/20/18.
+//  Copyright © 2018 DevSci. All rights reserved.
+//
+
+import XCTest
+@testable import ReloadAll
+
+class ReloadAllTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
